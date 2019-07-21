7d8bf2bcc0c1df9b238025a096b8c01057925f07
Melanie Freshness <david.alex.thompson@gmail.com>
2019-07-21 11:42:16 +0100

    Rest params

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,8 @@
 import '../styles/index.scss';
 
-console.log('HELLO WORLD!!!!!!');
-
-console.log(carId);
-
-var carId = 42; // not an error - carId just undefined before value is declared
-let carId2 = 100; // carId2 will throw error
-
-if (true) {
-    var foo = 9;
-    let foo2 = 10;
+function sendCars(day, ...allCarIds) {
+    allCarIds.forEach(id => console.log(id));
+    console.log(day);
 }
 
-console.log(foo, foo2); // foo will work, but let has block scoping so is undefined outside of the if statement
+sendCars('Monday', 100, 200, 555);
