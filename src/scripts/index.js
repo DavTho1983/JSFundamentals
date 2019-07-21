6b372e6edeccfdbbf05f528c0aa869bf30295317
Melanie Freshness <david.alex.thompson@gmail.com>
2019-07-21 15:38:12 +0100

    Scope

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,17 @@ import '../styles/index.scss';
 console.log((5 > 4) ? 'yes' : 'no');
 
 console.log(5 > 44 ? 'yes' : 'no');
+
+//Function and block scope
+
+function startCar(carId) {
+    let message = 'Starting...';
+    let startFn = function turnKey() {
+        let message = 'Override...';
+        console.log(message); //finds variable in parent function unless overridden locally
+    };
+    startFn();
+    console.log(message);
+}
+
+startCar(123);
