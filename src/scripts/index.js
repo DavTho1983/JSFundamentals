ad6e52d8ca6ff2e70a2c6be421b51ab6ec9f1fc6
Melanie Freshness <david.alex.thompson@gmail.com>
2019-07-21 14:52:15 +0100

    Comparisons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,22 +1,5 @@
 import '../styles/index.scss';
 
-console.log(Number.parseInt('55asd'));
-console.log(Number.parseFloat('55.88asd'));
+console.log((5 > 4) ? 'yes' : 'no');
 
-let i = 0;
-
-for (; i < 12; i++) {
-    if (i === 8) {
-        break;
-    }
-}
-
-console.log(i);
-
-for (let j = 0; j < 4; j++) {
-    if (j === 2) {
-        continue;
-    }
-
-    console.log(j);
-}
+console.log(5 > 44 ? 'yes' : 'no');
