6093fe78713676e6b1ff20a61578d11d2044ee9c
Melanie Freshness <david.alex.thompson@gmail.com>
2019-07-21 11:53:27 +0100

    Destructuring objects

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,12 @@
 import '../styles/index.scss';
 
-let cardIds = [1, 2, 5];
-let [car1, car2, car3] = cardIds;
+let car = {
+    id: 5000,
+    style: 'convertible'
+};
 
-console.log(car1, car2, car3);
+// let { id, style } = car;
+let id, style;
+({ id, style } = car);
 
-let cardIds2 = [1, 2, 5];
-let [cara, ...remainingCars] = cardIds2;
-
-console.log(cara, remainingCars);
-
-let carIds3 = [1, 7, 9];
-let remainingCars2;
-[, ...remainingCars2] = carIds3;
-
-console.log(remainingCars);
+console.log(id, style);
