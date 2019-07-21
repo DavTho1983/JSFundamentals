41e9dd788236904621a17f029a358ef94d5ee917
Melanie Freshness <david.alex.thompson@gmail.com>
2019-07-21 11:33:47 +0100

    const, var, let

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,15 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+console.log('HELLO WORLD!!!!!!');
+
+console.log(carId);
+
+var carId = 42; // not an error - carId just undefined before value is declared
+let carId2 = 100; // carId2 will throw error
+
+if (true) {
+    var foo = 9;
+    let foo2 = 10;
+}
+
+console.log(foo, foo2); // foo will work, but let has block scoping so is undefined outside of the if statement
