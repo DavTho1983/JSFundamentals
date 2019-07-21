84e52c6b2ff212522330adb2f660cef632a9329a
Melanie Freshness <david.alex.thompson@gmail.com>
2019-07-21 11:48:20 +0100

    Destructuring arrays

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,17 @@
 import '../styles/index.scss';
 
-function sendCars(day, ...allCarIds) {
-    allCarIds.forEach(id => console.log(id));
-    console.log(day);
-}
+let cardIds = [1, 2, 5];
+let [car1, car2, car3] = cardIds;
 
-sendCars('Monday', 100, 200, 555);
+console.log(car1, car2, car3);
+
+let cardIds2 = [1, 2, 5];
+let [cara, ...remainingCars] = cardIds2;
+
+console.log(cara, remainingCars);
+
+let carIds3 = [1, 7, 9];
+let remainingCars2;
+[, ...remainingCars2] = carIds3;
+
+console.log(remainingCars);
