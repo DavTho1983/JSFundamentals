5fdc96b96cde86ec955252979bbe8b0564daa1a9
Melanie Freshness <david.alex.thompson@gmail.com>
2019-07-23 07:26:19 +0100

    Class methods

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,13 @@ class Car {
     constructor(id) {
         this.id = id;
     }
+
+    identify(suffix) {
+        return `Car Id: ${this.id} ${suffix}`;
+    }
 }
 
 let car = new Car(123);
 car.id = 456;
 console.log(car.id);
+console.log(car.identify("!!!"));
