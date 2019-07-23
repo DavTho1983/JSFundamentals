bfe96a157eebf82cac9da84b77ee5f37e0b35054
Melanie Freshness <david.alex.thompson@gmail.com>
2019-07-23 07:38:50 +0100

    Modules

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,5 @@
 import "../styles/index.scss";
+import { Car } from "./models/car";
 
-class Vehicle {
-    constructor() {
-        this.type = "car";
-    }
-    start() {
-        return `Starting: ${this.type}`;
-    }
-}
-
-class Car extends Vehicle {
-    constructor() {
-        super();
-    }
-
-    start() {
-        return "in Car start. " + super.start();
-    }
-}
-
-let car = new Car();
-console.log(car.type);
-console.log(car.start());
+let car = new Car(123);
+console.log(car.id);
