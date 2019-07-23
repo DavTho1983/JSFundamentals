5420e4dd2ab0552f44e19a5eca6af291854b37a1
Melanie Freshness <david.alex.thompson@gmail.com>
2019-07-23 08:01:32 +0100

    Document model

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,29 @@
 import "../styles/index.scss";
 import { Car } from "./models/car";
 
-let car = new Car(123);
-console.log(car.id);
+console.log(window.innerWidth);
+console.log(innerWidth);
+
+let intervalID = setInterval(function() {
+    console.log("1 second passed");
+}, 1000);
+
+clearInterval(intervalID);
+
+console.log(location.href);
+console.log(document.location.href);
+
+let el = document.getElementById("first");
+let els = document.getElementsByClassName("p1");
+let els2 = document.getElementsByTagName("p");
+
+console.log(el);
+console.log(els[0]);
+console.log(els2);
+
+el.textContent = "New content...";
+el.setAttribute("foo", "fooValue");
+el.classList.add("p2");
+el.style.color = "magenta";
+
+console.log(el);
