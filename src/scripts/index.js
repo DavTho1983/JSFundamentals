8f1ee6a1bf905b7d26947531a2e886a2fde56650
Melanie Freshness <david.alex.thompson@gmail.com>
2019-07-24 07:42:24 +0100

    Promises

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,23 @@
 import "../styles/index.scss";
 import { Car } from "./models/car";
 
-try {
-    let car = newCar;
-} catch (error) {
-    console.log("error", error);
-} finally {
-    console.log("this always executes");
-}
+let promise = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 100, "somevalue");
+});
 
-console.log("continuing...");
+console.log(promise);
 
-try {
-    throw new Error("new custom error");
-} catch (error) {
-    console.log("error", error);
-} finally {
-    console.log("this always executes");
-}
+let promise2 = new Promise(function(resolve, reject) {
+    setTimeout(reject, 100, "someothervalue");
+});
+
+console.log(promise2);
+
+let promise3 = new Promise(function(resolve, reject) {
+    setTimeout(resolve, 100, "someValue");
+});
+
+promise3.then(
+    value => console.log("fulfilled:" + value),
+    error => console.log("rejected: " + error)
+);
