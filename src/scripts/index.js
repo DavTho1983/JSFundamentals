d5101237673d13fbe76480aa49f543cd3eac16a2
Melanie Freshness <david.alex.thompson@gmail.com>
2019-07-24 07:31:30 +0100

    try catch finally

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,20 @@
 import "../styles/index.scss";
 import { Car } from "./models/car";
 
-console.log(window.innerWidth);
-console.log(innerWidth);
+try {
+    let car = newCar;
+} catch (error) {
+    console.log("error", error);
+} finally {
+    console.log("this always executes");
+}
 
-let intervalID = setInterval(function() {
-    console.log("1 second passed");
-}, 1000);
+console.log("continuing...");
 
-clearInterval(intervalID);
-
-console.log(location.href);
-console.log(document.location.href);
-
-let el = document.getElementById("first");
-let els = document.getElementsByClassName("p1");
-let els2 = document.getElementsByTagName("p");
-
-console.log(el);
-console.log(els[0]);
-console.log(els2);
-
-el.textContent = "New content...";
-el.setAttribute("foo", "fooValue");
-el.classList.add("p2");
-el.style.color = "magenta";
-
-console.log(el);
+try {
+    throw new Error("new custom error");
+} catch (error) {
+    console.log("error", error);
+} finally {
+    console.log("this always executes");
+}
